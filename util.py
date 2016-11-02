4de45ff4e02443ac3cfc5305c2ae1d49b3313f7f
zhonglei_wang <zhonglei_wang@brown.edu>
2016-11-02 11:49:46 -0400

    DA finished & ImReader supports batch and vectorization now

--- util.py
+++ util.py
@@ -50,18 +50,19 @@ def select_img(pic_list, dir_path):
     pic_list: the txt filename containing target image names
     dir_path: the path holding images
     '''
-    if not os.path.exists(dir_path + "/" + "vol68"):
-        os.makedirs(dir_path + "/" + "vol68")
+    # if not os.path.exists(dir_path + "/" + "vol68"):
+    #     os.makedirs(dir_path + "/" + "vol68")
 
-    with open(pic_list, 'r') as list_file:
-        for img in list_file:
-            print 
+    # with open(pic_list, 'r') as list_file:
+    #     for img in list_file:
+    #         print 
+    pass
 
 
 class ImReader(object):
-    """
+    '''
     Iterator for reading in images from the given path 
-    """
+    '''
     def __init__(self, path):
         super(ImReader, self).__init__()
         if not os.path.exists(path):
@@ -69,13 +70,57 @@ class ImReader(object):
             return
 
         self.path = path
+        self.w = 0
+        self.h = 0
+        for sample in self.read():
+            self.h, self.w = sample.shape
+            break
+        self.features = self.w * self.h
 
 
-    def read(self, batch_sz = 1):
+    def read(self, batch_sz = 1, vector = False):
+        batch = []
         for file in os.listdir(self.path):
             if file.endswith(".png"):
-                yield imread(self.path + '/' + file)
-        
+                if vector:
+                    batch.append(np.reshape(imread(self.path + '/' + file), (1, -1)))
+                else:
+                    batch.append(imread(self.path + '/' + file))
+                
+                if len(batch) == batch_sz:
+                    res = batch
+                    batch = []
+                    yield res
+
+
+    def corrupt_and_read(self, batch_sz = 1, mean = 0, sigma = 50, vector = False):
+        x_batch, y_batch = [], []
+        for file in os.listdir(self.path):
+            if file.endswith(".png"):
+                original = imread(self.path + '/' + file)
+                if vector:
+                    x_batch.append(np.reshape(self.gaussian_noise(original, mean, sigma), (1, -1)))
+                    y_batch.append(np.reshape(original, (1, -1)))
+                else:
+                    x_batch.append(self.gaussian_noise(original, mean, sigma))
+                    y_batch.append(original)
+                if len(x_batch) == batch_sz:
+                    res_x, res_y = x_batch, y_batch
+                    x_batch, y_batch = [], []
+                    yield res_x, res_y
+
+    
+    def gaussian_noise(self, img, mean = 0, sigma = 0.5):
+        '''
+        Apply Gaussian noise to the img.
+
+        img: original image
+        mean: mean of the Gaussian func
+        sigma: sigma of the Gaussian func
+
+        return: corrupted image
+        '''
+        return img + np.random.normal(mean, sigma, img.shape)
 
 
 if __name__ == '__main__':
