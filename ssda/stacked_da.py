da89f64a89b463190330417d0eba401128b2154e
zhonglei_wang <zhonglei_wang@brown.edu>
2016-11-07 15:19:02 -0500

    update finetuning net

--- ssda/stacked_da.py
+++ ssda/stacked_da.py
@@ -33,15 +33,16 @@ class StackedDenoisingAutoEncoder(object):
         self.b_ = [da.b_ for da in self.das]
 
         # Feed-forward step
+        self.layer_nodes = []
         next_input = self.input_corrupt
         for i in xrange(len(self.w)):
-            self.h = tf.matmul(next_input, self.w[i]) + self.b[i]
-            self.h_ = tf.matmul(self.h, self.w_[i]) + self.b_[i])
-            next_input = self.h_
+            h = tf.nn.sigmoid(tf.matmul(self.input_corrupt, self.w[i]) + self.b[i])
+            self.layer_nodes.append(h)
+            h_ = tf.nn.sigmoid(tf.matmul(h, self.w_[i]) + self.b_[i])
+            self.layer_nodes.append(h_)
+            next_input = h_
 
-        # for i in xrange(len(self.w)):
-        #     self.h = tf.nn.sigmoid(tf.matmul(self.input_corrupt, self.w) + self.b)
-        #     self.h_ = tf.nn.sigmoid(tf.matmul(self.h, self.w_) + self.b_)
+        self.h_ = self.layer_nodes[-1]
 
         # Loss func
         self.error = tf.sqrt(tf.reduce_mean(tf.square(self.input_original - self.h_)))
@@ -62,12 +63,12 @@ class StackedDenoisingAutoEncoder(object):
                     next_input = [x,y]
                     for j in xrange(i):
                         da = self.das[j]
-                        next_input[0] = da.sess.run([da.h_], feed_dict = {da.input_corrupt: next_input[0]})
-                        next_input[1] = da.sess.run([da.h_], feed_dict = {da.input_corrupt: next_input[1]})
+                        next_input[0] = da.sess.run([da.h], feed_dict = {da.input_corrupt: next_input[0]})
+                        next_input[1] = da.sess.run([da.h], feed_dict = {da.input_corrupt: next_input[1]})
                     da = self.das[i]
                     da.sess.run([da.train_step], feed_dict = {
-                    	da.input_corrupt: next_input[0], 
-                    	da.input_original: next_input[1]})
+                        da.input_corrupt: next_input[0], 
+                        da.input_original: next_input[1]})
 
 
 
